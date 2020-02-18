5f26cef946d9e67c6db90de4fbd1e07f3c4fc5d9
Louis Young <me@louisyoung.co.uk>
2020-02-18 20:20:10 +0000

    Adding animation

--- public/src/scripts/animations.js
+++ public/src/scripts/animations.js
@@ -5,9 +5,15 @@
  */
 
 const animations = () => {
-  const wow = new WOW(); // eslint-disable-line no-undef
-
-  wow.init();
+  AOS.init({ once: true }); // eslint-disable-line no-undef
 };
 
 animations();
+
+// const elements = document.querySelectorAll("[data-animate]");
+
+// elements.forEach(element => {
+//   element.dataset.aos = "fade-up"; // eslint-disable-line no-param-reassign
+//   element.dataset.aosDuration = "1000"; // eslint-disable-line no-param-reassign
+//   element.dataset.aosOffset = "750"; // eslint-disable-line no-param-reassign
+// });

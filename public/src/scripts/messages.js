2e6ea6fd08cb241773cf1cac36f7e99e90659f56
Louis Young <me@louisyoung.co.uk>
2021-01-03 12:38:04 +0000

    Update animations

--- public/src/scripts/messages.js
+++ public/src/scripts/messages.js
@@ -1,18 +1,9 @@
 console.clear();
 
-console.log(
-  "%c 👋 Hello, Developer!",
-  "color: #bada55; font-size:1.5rem; font-weight: 500"
-);
+console.log("%c 👋 Hello, Developer!", "color: #bada55; font-size:1.5rem; font-weight: 500");
 
-console.log(
-  "%c 🐛 Found a bug? Please let me know via my email, GitHub issues, etc...",
-  "font-size:1rem"
-);
+console.log("%c 🐛 Found a bug? Please let me know via my email, GitHub issues, etc...", "font-size:1rem");
 
-console.log(
-  "%c 🛠 My website is open source: https://github.com/louis-young/portfolio",
-  "font-size:1rem"
-);
+console.log("%c 🛠 My website is open source: https://github.com/louis-young/portfolio", "font-size:1rem");
 
 console.log("%c Enjoy looking around!", "font-size:1rem");
